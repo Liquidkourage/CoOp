3e04c980dc8a59adb43481e55b1176850bf8cbec
liquidkourage <liquidkouragekaraoke@gmail.com>
2025-12-13 14:36:04 -0600

    Configure Next.js to exclude pg from client bundle

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverComponentsExternalPackages: ['pg'],
+  experimental: {
+    serverComponentsExternalPackages: ['pg'],
+  },
 }
 
 module.exports = nextConfig
-
