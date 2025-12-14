65f3dae200251a98465cad25c5522fdcf0b41f37
liquidkourage <liquidkouragekaraoke@gmail.com>
2025-12-13 21:17:56 -0600

    Fix syntax error - missing closing parenthesis in map return statement

--- app/page.tsx
+++ app/page.tsx
@@ -359,7 +359,8 @@ export default function Home() {
                   </div>
                 )}
               </div>
-            ))}
+              );
+            })}
           </div>
         )}
       </main>
